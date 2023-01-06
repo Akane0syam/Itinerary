0ede5e23b80b03462cbd790b7391819c74dcbae5
EC2 Default User <ec2-user@ip-172-31-41-67.ap-northeast-1.compute.internal>
2023-01-06 14:31:17 +0000

    [plans(new/index/show/edit)]

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
+  
   root to: 'homes#top'
   # URL /users/sign_in ...
 devise_for :users, controllers: {
   registrations: "user/registrations",
   sessions: 'user/sessions'
 }
- namespace :user do
-    resources :users, only: [:index, :show, :edit, :update]
-  end
+ 
+  resources :users, only: [:index, :show, :edit, :update]
+  resources :plans, only: [:new, :index, :show, :edit, :update, :create, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
